2340510ab0d0a8754b48a6b46ca06f5b4b5b5265
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-02-09 23:08:29 +0530

    new

---
+++ helloworld.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+   <head>
+        <title>
+            Hellow World
+        </title>
+    </head>
+    
+<body>
+
+<?php
+    echo "This is the first PHP";
+    $input = $_GET["str"];
+    echo "The input is : ",$input;
+?>
+
+<?php
+  echo "<br>This is the second PHP";
+?>
+
+</body>
+</html>
--- variables and its scope.php
+++ variables and its scope.php
@@ -1,7 +1,57 @@
 <?php
 
+								/*Declaring the variables*/
+$name = "Prashanth";
+$age = 20;
+$isMale = true;
+$salary = null;
+$height = 1.85;    
+
+								/* Print the values of the variable */
+echo "Value of the variable";
+echo $name.'<br>';
+echo $age;
+echo "<br> $isMale";
+echo "<br> $salary";
+echo "<br> $height";
+
+ 								/* Print the type of variable */
+echo "<br>Type of the variable<br>";
+echo gettype($name).'<br>';
+echo gettype($age).'<br>';
+echo gettype($isMale).'<br>';
+echo gettype($salary).'<br>';
+echo gettype($height).'<br>';  
+
+ 							/* Print the whole variable */
+var_dump($name,$age,$isMale,$salary,$height);
+
+ 							/* Change the value of the variable */	
+echo "<br>Before : ",gettype($name).'<br>';
+$name = false;   /*before name was having Prashanth, but now changed to false*/
+echo "After : ",gettype($name).'<br>';
+
+ 							/*   Variable checking function  */
+is_string($name);  // false
+is_int($age);   // true
+is_bool($isMale);  // true
+is_double($height); // true
+
+ 						/*Check if the variable is defined or not*/
+isset($name);       // true
+isset($address);    // false
+
+								/*Constants*/
+define('PI',3.14);
+echo PI.'<br>';
+
+						/*Using PHP buit-in functions*/
+echo SORT_ASC.'<br>';
+echo PHP_INT_MAX.'<br>';
+echo PHP_INT_MIN.'<br>';
+
 ##############################################################################
-					/*Global variable*/
+						/*Global variable*/
 $x = 24;
 function test1() {
 	echo "Global variable x=$x cannot be accessed within a function";
